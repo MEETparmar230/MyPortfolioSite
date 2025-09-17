7ee6059ad5c69cc782872fe1aec64bb317b8dbdb
meet <meetparmar2362004@gmail.com>
2025-09-17 16:26:08 +0530

    logo aded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -20,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "PortFolio",
   description: "Enthusiastic and detail-oriented MERN Stack Developer with hands-on experience building frontend and full stack applications. Skilled in developing responsive web interfaces using React and Tailwind CSS, and backend APIs with Node.js and MongoDB. Eager to contribute to dynamic development teams and grow in a collaborative environment.",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -28,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-     <html lang="en" className="h-full">
+    <html lang="en" className="h-full">
       <body className={`${geistSans.variable} ${geistMono.variable} h-full flex flex-col bg-black antialiased`}>
         <AnimatedGradient />
         <Navbar />
